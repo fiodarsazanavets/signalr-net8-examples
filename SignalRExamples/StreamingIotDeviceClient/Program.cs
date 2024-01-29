99488ec2f458ca366f7a141f061935c10604e191
empowerFiodar <fiodar@empower.me>
2024-01-29 14:22:13 +0000

    Add a streaming .NET client

---
+++ SignalRExamples/StreamingIotDeviceClient/Program.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.SignalR.Client;
+using System.Threading.Channels;
+
+string url = "https://localhost:5001/devicesHub";
+string deviceName = "Device1";
+
+HubConnection hubConnection = new HubConnectionBuilder()
+                         .WithUrl(url)
+                         .Build();
+
+await hubConnection.StartAsync();
+
+Channel<string> channel = Channel.CreateUnbounded<string>();
+await hubConnection.SendAsync(
+    "BroadcastInitializationSequence", channel.Reader);
+
+await channel.Writer.WriteAsync($"Device {deviceName} started");
+await channel.Writer.WriteAsync($"Device {deviceName} is loading...");
+await channel.Writer.WriteAsync($"Device {deviceName} loaded successfully");
+await channel.Writer.WriteAsync($"Device {deviceName} registration started");
+
+CancellationTokenSource cancellationTokenSource = new();
+IAsyncEnumerable<string> stream = hubConnection
+    .StreamAsync<string>("RegisterDevice",
+    deviceName, cancellationTokenSource.Token);
+
+await foreach (var reply in stream)
+{
+    Console.WriteLine(reply);
+}
+
+
+await channel.Writer.WriteAsync($"Device {deviceName} registered successfully");
+channel.Writer.Complete();
+
+Console.WriteLine("Press any key to exit.");
+Console.ReadKey();
