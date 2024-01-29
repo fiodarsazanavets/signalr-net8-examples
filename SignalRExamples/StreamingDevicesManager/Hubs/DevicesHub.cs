fa406fb60ce79c73863db14e05ceacc2370e8dc9
empowerFiodar <fiodar@empower.me>
2024-01-29 14:06:22 +0000

    Add an example of a streaming SignalR server

---
+++ SignalRExamples/StreamingDevicesManager/Hubs/DevicesHub.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System.Runtime.CompilerServices;
+
+namespace StreamingDevicesManager.Hubs;
+
+public class DevicesHub : Hub
+{
+    public async Task BroadcastInitializationSequence(IAsyncEnumerable<string> stream)
+    {
+        await foreach (var item in stream)
+        {
+            await Clients.All.SendAsync("ReceiveMessage", item);
+        }
+    }
+
+    public async IAsyncEnumerable<string> RegisterDevice(
+        string deviceName,
+        [EnumeratorCancellation]
+        CancellationToken cancellationToken)
+    {
+        yield return $"Request received from {deviceName} device";
+        yield return $"Device registration started for the {deviceName} device";
+        await Task.Delay(1000, cancellationToken);
+        yield return $"Device registration completed for the {deviceName} device";
+        yield return $"Device {deviceName} is ready";
+    }
+
+    public async IAsyncEnumerable<string> RestartSystem(
+        [EnumeratorCancellation]
+        CancellationToken cancellationToken)
+    {
+        yield return "System restart requested";
+        yield return "System restarting...";
+        await Task.Delay(1000, cancellationToken);
+        yield return "System restart complete";
+        yield return "System is fully operational";
+    }
+}
---
+++ SignalRExamples/StreamingDevicesManager/wwwroot/js/site.js
@@ -0,0 +1,39 @@
+﻿const connection = new signalR.HubConnectionBuilder()
+    .withUrl("/devicesHub")
+    .configureLogging(signalR.LogLevel.Information)
+    .build();
+
+connection.on("ReceiveMessage", (message) => {
+    $('#devices-panel')
+        .prepend($('<div />').text(message));
+});
+
+$('#btn-restart').click(function () {
+    connection.stream("RestartSystem")
+        .subscribe({
+            next: (message) => $('#devices-panel')
+                .prepend($('<div />')
+                    .text(message))
+        });
+});
+
+async function start() {
+    try {
+        await connection.start();
+        var subject = new signalR.Subject();
+        connection.send("BroadcastInitializationSequence", subject)
+            .catch(err => console.error(err.toString()));
+        subject.next("Device Manager is connecting");
+        subject.next("Device Manager is connected");
+        subject.complete();
+    } catch (err) {
+        console.log(err);
+        setTimeout(() => start(), 5000);
+    }
+};
+
+connection.onclose(async () => {
+    await start();
+});
+
+start();
